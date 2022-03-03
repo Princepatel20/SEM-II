23976548c6bee7501936f7c8a9ceb2bebcecc3f5
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2022-03-04 01:06:10 +0530

    JAVA

---
+++ VolumeOfCube.java
@@ -0,0 +1,22 @@
+//Write Programme to calculate the volume tof cube.
+import java.util.*;
+public class VolumeOfCube
+{
+    public static void main(String[]args)
+    {
+        double a,vol;
+        Scanner sc = new Scanner(System.in)
+        System.out.println("Enter Length of Cube A: ");
+        a=sc.nextDouble();
+
+        //logic
+        vol=a*a*a;
+
+        System.out.println("\n");
+
+        //result
+        System.out.println("
+        Volume of Cube of Length "+a + ":" +vol);
+        )
+    }
+}
