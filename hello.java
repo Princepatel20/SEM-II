5d0379c09b5728d38af926a2d75d7af9d8dd3cc7
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2022-03-05 19:12:06 +0530

    Delete hello.java

--- hello.java
+++
@@ -1,9 +0,0 @@
-//Write Programme that display hello world on screen.
-public class hello
-{
-        public staticvoid main(String[] args)
-            {
-                System.out.println("
-                Hello World!")
-            }
-}
