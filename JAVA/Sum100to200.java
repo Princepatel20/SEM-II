140e5090104c264da81479a8b7d72be3d18f69c2
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2022-03-18 10:57:47 +0530

    Update Sum100to200.java

--- JAVA/Sum100to200.java
+++ JAVA/Sum100to200.java
@@ -8,7 +8,7 @@ public class Sum100to200
         System.out.println("The number between 100 and 200 and are divisible by 5.");
         for ( i = 101; i < 200; i++)
         {
-            if(i%5=0)
+            if(i%5==0)
             {
                 System.out.print(""+i);
                 sum+=i;
