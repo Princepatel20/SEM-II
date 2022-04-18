dd48d85a6278e40d99687559a3f6d404be937d13
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2022-04-18 14:30:23 +0530

    Add files via upload

---
+++ JAVA/function.java
@@ -0,0 +1,19 @@
+public class function
+{
+	public static void findsum(int n)
+	{
+		int i,sum=0;
+		for(i=1;i<n;i++)
+		{
+			sum=sum+i;			
+		}
+		System.out.println("Sum is"+sum);	
+	}
+
+	public static void main (String arg[])
+	{
+	findsum(100);
+
+                  }
+}
+
