904b556dc83a958a648b496379a789cfc3fb4ef2
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2022-07-11 12:06:36 +0530

    Add files via upload

---
+++ JAVA/program2.java
@@ -0,0 +1,22 @@
+public class program2
+{
+	public static void main(String args[])
+	{	
+			System.out.println("Hello World");
+
+			System.out.println("Prince");
+
+			System.out.println("Gondaliya");
+			System.out.println("Surat");
+
+			System.out.println(args[0]);
+			System.out.println(args[1]);
+			System.out.println(args[2]);
+			System.out.println(args[3]);
+	}
+
+
+
+
+}
+
